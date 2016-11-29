c4b7f496b9f7700b7485a6e667c0382d32ba9e61
Mateusz <matstanik@gmail.com>
2016-11-29 12:45:55 +0100

    Create unit test

---
+++ UnitTestClearCode/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace UnitTestClearCode
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
